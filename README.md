1503553ab4f115dd9b2c2abe35687609191c26ce
EvaDugina <vania.dugin@bk.ru>
2025-06-04 18:15:32 +0300

    feat: All is ready

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ pip install .
 ```
 ### Usage
 ```
-codecheck [-h] -c config.json [-f .clang-format] files [files ...]
+codecheck [-h] -c config.json [files ...]
 ```     
 | Argument     | Description |
 | ------------ | ----------- |
@@ -20,6 +20,46 @@ codecheck [-h] -c config.json [-f .clang-format] files [files ...]
 | -c, --conf   | path to JSON configuration file (default: None) |
 | -f, --format | path to clang-format configuration (default: None) |
 
-###
+### Примеры корректного вывода программы
+
+```commandline
+# codecheck
+Запуск автоматической проверки...
+BUILD checked!
+CPPCHECK checked!
+CLANG-FORMAT checked!
+VALGRIND checked!
+COPYDETECT checked!
+CATCH2 checked!
+Автоматичекая проверка завершена!
+====
+время: 28.0 сек
+```
+
+```commandline
+# codecheck -c examples/config_all_console.json examples/code_example_1.c     
+Запуск автоматической проверки...
+BUILD checked!
+CPPCHECK checked!
+CLANG-FORMAT checked!
+COPYDETECT checked!
+VALGRIND checked!
+CATCH2 checked!
+Автоматичекая проверка завершена!
+====
+время: 28.4 сек
+```
+
+### Запуск Docker
 docker build -t codecheck .
 docker run -it --rm -v ./:/tmp codecheck
+
+----
+
+# Перспективы
+
+ISSUES:
+- Реализовать многопоточку для Catch2
+- Заменить все запуски либо на ```subproccess.popen()``` либо на ```subproccess.run()```
+- Отрефакторить Checkers и Checker
+- Сделать красивый Readme с примерами вывода, инструкциями, флагами для проверок
--- codecheck/__main__.py
+++ codecheck/__main__.py
@@ -6,42 +6,42 @@ from .tools_config import TOOLS_CHECKERS
 
 def main():
 
-    # parser = argparse.ArgumentParser(prog="codecheck",
-    #     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument("files", metavar="files", nargs="+",
-    #                 help="paths to test files to check")
-    # parser.add_argument("-c", "--conf", metavar="config.json",
-    #                     help="path to JSON configuration file", required=True)
-    # parser.add_argument("-f", "--format", metavar=".clang-format",
-    #                     help="path to clang-format configuration")
-    # args = parser.parse_args()
-    #
-    # if not args.conf:
-    #     parser.error("path to configuration json file must be provided"
-    #                  "specify it with -c or --conf flags")
-    # if not args.files:
-    #     parser.error("path to files to check must be provided"
-    #                  "specify it with -t or --test flags")
-    #
-    # config = {
-    #     "config_json": args.conf,
-    #     "files_to_check": args.files,
-    # }
+    parser = argparse.ArgumentParser(prog="codecheck",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("files", metavar="files", nargs="+",
+                    help="paths to test files to check")
+    parser.add_argument("-c", "--conf", metavar="config.json",
+                        help="path to JSON configuration file", required=True)
+    parser.add_argument("-f", "--format", metavar=".clang-format",
+                        help="path to clang-format configuration")
+    args = parser.parse_args()
 
-    # Debugging on Windows
+    if not args.conf:
+        parser.error("path to configuration json file must be provided"
+                     "specify it with -c or --conf flags")
+    if not args.files:
+        parser.error("path to files to check must be provided"
+                     "specify it with -t or --test flags")
+
+    config = {
+        "config_json": args.conf,
+        "files_to_check": args.files,
+    }
+
+    # Debugging in IDE
     # config = {
     #     "config_json": '../examples/config_all_debug.json',
     #     "files_to_check": ["../examples/code_example.c"],
     # }
 
     # Console Testing
-    config = {
-        "config_json": 'examples/config_all_console.json',
-        "files_to_check": [
-            "examples/code_example_1.c", "examples/code_example_2.c",
-            "examples/code_example_3.c", "examples/code_example_4.c"
-        ],
-    }
+    # config = {
+    #     "config_json": 'examples/config_all_console.json',
+    #     "files_to_check": [
+    #         "examples/code_example_1.c", #"examples/code_example_2.c",
+    #         #"examples/code_example_3.c", "examples/code_example_4.c"
+    #     ],
+    # }
 
     start(TOOLS_CHECKERS, config["config_json"], config["files_to_check"])
     
